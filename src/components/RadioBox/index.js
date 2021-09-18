7e58e91352dbc90d10f770fc0b1922ddff93a2e9
nguyendangdinh47 <nguyendangdinh47@gmail.com>
2021-09-18 21:55:10 +0700

    UI/component Radiobox Field

---
+++ src/components/RadioBox/index.js
@@ -0,0 +1,48 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { FormControlLabel, Radio, RadioGroup } from '@material-ui/core';
+import { Controller } from 'react-hook-form';
+
+RadioField.propTypes = {
+  form: PropTypes.object.isRequired,
+  name: PropTypes.string.isRequired,
+  label: PropTypes.string,
+  listSelect: PropTypes.array.isRequired,
+};
+
+function RadioField(props) {
+  console.log(props.listSelect);
+  const { form, name, inline, defaultValue, dataList } = props;
+  const {
+    formState: { errors },
+  } = form;
+  const hasError = errors[name];
+
+  return (
+    <Controller
+      name={name}
+      control={form.control}
+      render={({
+        field: { onChange, onBlur, value, name, ref },
+        fieldState: { invalid, isTouched, isDirty, error },
+        formState,
+      }) => (
+        <RadioGroup
+          onChange={onChange}
+          name={name}
+          value={value}
+          row={inline}
+          defaultValue={defaultValue}
+          error={!!hasError}
+          helperText={errors[name]?.message}
+        >
+          {dataList.map((item) => (
+            <FormControlLabel value={item.value} control={<Radio />} label={item.name} />
+          ))}
+        </RadioGroup>
+      )}
+    />
+  );
+}
+
+export default RadioField;
